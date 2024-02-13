e0fe847a741b7c03f5f8668cff418648a9086a20
Admiral-Simo <admiralsimo16@gmail.com>
2024-02-13 21:55:11 +0100

    setup routing system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import HomeScreen from "./pages/HomeScreen";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  {
+    path: "/currency",
+    element: <div>something right here the currency page</div>,
+  },
+]);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);

2572895faba48f24e0ae436ca69dea2460f12f51
Admiral-Simo <admiralsimo16@gmail.com>
2024-02-16 23:33:27 +0100

    add basic crypto info in currency screen

--- src/pages/CurrencyScreen.jsx
+++ src/pages/CurrencyScreen.jsx
@@ -1,7 +1,29 @@
 import { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import Spin from "../components/Spin";
+import Change24 from "../components/Change24";
 import useFetch from "../hooks/useFetch";
+import DisplayPrice from "../components/DisplayPrice";
+import { numberWithCommas } from "../utils/numberUtils";
+
+// interface CoinData {
+//     uuid: string;
+//     symbol: string; used
+//     name: string; used
+//     color: string; not used
+//     iconUrl: string; used
+//     marketCap: string; used
+//     price: string; used
+//     listedAt: number;
+//     tier: number;
+//     change: string; used
+//     rank: number;
+//     sparkline: string[];
+//     lowVolume: boolean;
+//     coinrankingUrl: string;
+//     '24hVolume': string; used
+//     contractAddresses: string[];
+// }
 
 function CurrencyScreen() {
   const { constData } = useFetch();
@@ -14,12 +36,49 @@ function CurrencyScreen() {
   }, [uuid, constData]);
 
   return (
-    <div>
-      {JSON.stringify(currentCurrency)}
-      {!constData?.coins && (
+    <div className="pt-8 max-w-screen-xl mx-auto">
+      {!constData?.coins ? (
         <div className="w-full flex justify-center items-center my-20">
           <Spin />
         </div>
+      ) : (
+        <>
+          <div className="flex items-center gap-3">
+            <img className="w-8" src={currentCurrency?.iconUrl} alt="coin" />
+            <h1 className="font-bold">
+              {currentCurrency?.name}
+              <span className="ml-0.5 font-light">
+                {currentCurrency?.symbol}
+              </span>
+            </h1>
+          </div>
+          <div className="flex items-center gap-3">
+            {Number(currentCurrency?.price) >= 2 ? (
+              <p className="font-bold text-3xl">
+                ${numberWithCommas(Number(currentCurrency?.price).toFixed(2))}
+              </p>
+            ) : (
+              <p>${Number(currentCurrency?.price).toFixed(6)}</p>
+            )}
+            <Change24 change={currentCurrency?.change} />
+          </div>
+          <div className="flex items-center gap-3">
+            <h1 className="text-2xl font-bold capitalize">
+              marketcap:{" "}
+              <span className="text-green-300">
+                ${numberWithCommas(currentCurrency?.marketCap)}
+              </span>
+            </h1>
+          </div>
+          <div className="flex items-center gap-3">
+            <h1 className="text-2xl font-bold capitalize">
+              24 hours volume:{" "}
+              <span className="text-green-300">
+                ${numberWithCommas(currentCurrency["24hVolume"])}
+              </span>
+            </h1>
+          </div>
+        </>
       )}
     </div>
   );

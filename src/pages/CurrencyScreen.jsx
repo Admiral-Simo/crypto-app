404b08eb8a78a9f9e168b5918ec6e453c86aa9da
Admiral-Simo <admiralsimo16@gmail.com>
2024-02-16 23:58:35 +0100

    complete currency page

--- src/pages/CurrencyScreen.jsx
+++ src/pages/CurrencyScreen.jsx
@@ -3,27 +3,16 @@ import { useParams } from "react-router-dom";
 import Spin from "../components/Spin";
 import Change24 from "../components/Change24";
 import useFetch from "../hooks/useFetch";
-import DisplayPrice from "../components/DisplayPrice";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 import { numberWithCommas } from "../utils/numberUtils";
 
-// interface CoinData {
-//     uuid: string;
-//     symbol: string; used
-//     name: string; used
-//     color: string; not used
-//     iconUrl: string; used
-//     marketCap: string; used
-//     price: string; used
-//     listedAt: number;
-//     tier: number;
-//     change: string; used
-//     rank: number;
-//     sparkline: string[];
-//     lowVolume: boolean;
-//     coinrankingUrl: string;
-//     '24hVolume': string; used
-//     contractAddresses: string[];
-// }
+function SparklineChart({ data }) {
+  return (
+    <Sparklines data={data} width={100} height={35} margin={5}>
+      <SparklinesLine color="#1c8cdc" />
+    </Sparklines>
+  );
+}
 
 function CurrencyScreen() {
   const { constData } = useFetch();
@@ -36,7 +25,7 @@ function CurrencyScreen() {
   }, [uuid, constData]);
 
   return (
-    <div className="pt-8 max-w-screen-xl mx-auto">
+    <div className="pt-8 px-2 max-w-screen-xl mx-auto space-y-2">
       {!constData?.coins ? (
         <div className="w-full flex justify-center items-center my-20">
           <Spin />
@@ -44,40 +33,63 @@ function CurrencyScreen() {
       ) : (
         <>
           <div className="flex items-center gap-3">
-            <img className="w-8" src={currentCurrency?.iconUrl} alt="coin" />
-            <h1 className="font-bold">
+            <img
+              className="w-8 md:w-16"
+              src={currentCurrency?.iconUrl}
+              alt="coin"
+            />
+            <h1 className="font-bold md:text-3xl">
               {currentCurrency?.name}
               <span className="ml-0.5 font-light">
                 {currentCurrency?.symbol}
               </span>
             </h1>
           </div>
-          <div className="flex items-center gap-3">
+          <div className="flex justify-between items-center gap-3 w-full">
             {Number(currentCurrency?.price) >= 2 ? (
               <p className="font-bold text-3xl">
                 ${numberWithCommas(Number(currentCurrency?.price).toFixed(2))}
               </p>
             ) : (
-              <p>${Number(currentCurrency?.price).toFixed(6)}</p>
+              <p className="font-bold text-3xl">
+                ${Number(currentCurrency?.price).toFixed(6)}
+              </p>
             )}
             <Change24 change={currentCurrency?.change} />
           </div>
-          <div className="flex items-center gap-3">
-            <h1 className="text-2xl font-bold capitalize">
-              marketcap:{" "}
+          <div className="flex items-center gap-3 w-full">
+            <h1 className="text-2xl font-bold flex items-center justify-between w-full capitalize">
+              marketcap:
               <span className="text-green-300">
                 ${numberWithCommas(currentCurrency?.marketCap)}
               </span>
             </h1>
           </div>
-          <div className="flex items-center gap-3">
-            <h1 className="text-2xl font-bold capitalize">
+          <div className="flex items-center gap-3 w-full">
+            <h1 className="text-2xl font-bold flex items-center justify-between w-full capitalize">
               24 hours volume:{" "}
               <span className="text-green-300">
                 ${numberWithCommas(currentCurrency["24hVolume"])}
               </span>
             </h1>
           </div>
+
+          <h1 className="text-2xl font-bold capitalize">Contract Address: </h1>
+          <div className="flex items-center gap-3 flex-wrap">
+            {currentCurrency?.contractAddresses.length ? (
+              currentCurrency?.contractAddresses.map((item) => (
+                <div className="rounded-md py-0.5 px-1 bg-green-900 text-green-200">
+                  {item}
+                </div>
+              ))
+            ) : (
+              <div className="rounded-md py-0.5 px-1 bg-red-900 text-red-200">
+                No address available
+              </div>
+            )}
+          </div>
+          <h1 className="text-2xl font-bold capitalize">Chart: </h1>
+          <SparklineChart data={currentCurrency?.sparkline} />
         </>
       )}
     </div>
